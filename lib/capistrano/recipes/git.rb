faeb9af4da9df13e081064b281e466f907796f42
Fernando Aleman <fernandoaleman@mac.com>
2013-05-18 20:37:24 -0500

    Added git recipe

---
+++ lib/capistrano/recipes/git.rb
@@ -0,0 +1,119 @@
+module CapistranoRecipes
+  module Git
+    def self.load_into(configuration)
+      configuration.load do
+        default_run_options[:pty]   = true
+        ssh_options[:forward_agent] = true
+
+        set(:repository)        { abort "Please specify repository, set :repository, 'foo'" }
+        
+        _cset :branch,          'master'
+        _cset :use_sudo,        false
+        _cset :check_repo,      true
+        
+        set :migrate_target,    :current
+
+        set(:latest_release)    { fetch(:current_path) }
+        set(:release_path)      { fetch(:current_path) }
+        set(:releases_path)     { fetch(:current_path) }
+        set(:current_release)   { fetch(:current_path) }
+        set(:current_revision)  { capture("cd #{deploy_to} && git rev-parse --short HEAD").strip }
+        set(:latest_revision)   { capture("cd #{deploy_to} && git rev-parse --short HEAD").strip }
+        set(:previous_revision) { capture("cd #{deploy_to} && git rev-parse --short HEAD@{1}").strip }
+
+        set :local_branch do
+          `git symbolic-ref HEAD 2> /dev/null`.strip.sub('refs/heads/', '')
+        end
+        
+        after 'deploy:setup' do
+          run "git clone --no-checkout #{repository} #{current_path}"
+        end
+        
+        namespace :deploy do
+          desc 'Deploy and start a cold application'
+          task :cold do
+            update
+            migrate
+          end
+
+          desc 'Update the deployed code through transaction to rollback if it has errors'
+          task :update do
+            transaction do
+              update_code
+            end
+          end
+
+          desc 'Update the deployed code'
+          task :update_code, :except => { :no_release => true } do
+            run "cd #{deploy_to} && git fetch origin && git reset --hard origin/#{branch}"
+            finalize_update
+          end
+          
+          desc 'Run migrations'
+          task :migrate, :roles => :db, :only => { :primary => true } do
+            run "cd #{deploy_to} && RAILS_ENV=#{rails_env} #{rake} db:migrate"
+          end
+
+          desc 'Deploy and run migrations'
+          task :migrations do
+            update
+            migrate
+            restart
+          end
+
+          namespace :rollback do
+            desc 'Move repo back to the previous version of HEAD'
+            task :repo, :except => { :no_release => true } do
+              set :branch, "HEAD@{1}"
+              deploy.default
+            end
+
+            desc 'Rewrite reflog so HEAD@{1} will continue to point to the next previous release'
+            task :cleanup, :except => { :no_release => true } do
+              run "cd #{deploy_to}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+            end
+
+            desc 'Roll back to the previously deployed version'
+            task :default do
+              rollback.repo
+              rollback.cleanup
+            end
+          end
+
+          namespace :pending do
+            task :diff, :except => { :no_release => true } do
+              # nothing
+            end
+
+            desc 'Show pending commits'
+            task :default do
+              system("git log --pretty=medium --stat #{current_revision}..origin/#{branch}")
+            end
+          end
+
+          desc 'Check that local git repo is in sync with remote'
+          task :check_repo do
+            unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+              puts "WARNING: HEAD is not the same as origin/#{branch}"
+              puts "Run `git push origin #{branch}` to sync changes."
+              exit
+            end if fetch(:check_repo)
+          end
+          %w[deploy deploy:cold deploy:migrations].each do |task|
+            before "#{task}", "deploy:check_repo"
+          end
+          
+          task :symlink do
+            # nothing
+            # if task is not being used, it will not appear in `cap -T`
+          end
+          
+          task :create_symlink do
+            # nothing
+            # if task is not being used, it will not appear in `cap -T`
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/git_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe 'git' do
+  before do
+    mock_config do
+      use_recipe :git
+      set :deploy_to, '/foo/bar'
+      def finalize_update; return; end;
+    end
+  end
+
+  it 'has branch' do
+    config.branch.should == 'master'
+  end
+
+  context 'with repository' do
+    before do
+      mock_config { set :repository, 'git@example.com/test-app.git' }
+    end
+
+    describe 'deploy:setup' do
+      before do
+        mock_config do
+          set :shared_path, "#{deploy_to}/shared"
+          set :shared_children, %w(public/system log tmp/pids)
+        end
+      end
+
+      it 'clones repository' do
+        cli_execute 'deploy:setup'
+        config.should have_run('git clone --no-checkout git@example.com/test-app.git /foo/bar/current')
+      end
+    end
+
+    describe 'deploy:update' do
+      it 'updates' do
+        cli_execute 'deploy:update'
+        config.should have_run('cd /foo/bar && git fetch origin && git reset --hard origin/master')
+      end
+    end
+  end
+
+  it 'has current revision' do
+    config.should_receive(:capture).with('cd /foo/bar && git rev-parse --short HEAD') { "baz\n" }
+    config.current_revision.should == 'baz'
+  end
+
+  it 'shows pending' do
+    config.should_receive(:current_revision) { 'baz' }
+    config.namespaces[:deploy].namespaces[:pending].should_receive(:system).with('git log --pretty=medium --stat baz..origin/master')
+    cli_execute 'deploy:pending'
+  end
+
+  it 'sets forward agent' do
+    config.ssh_options[:forward_agent].should == true
+  end
+end
